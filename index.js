571f934b58317d96475a9951c2edf4e0c937be84
Sergio Duran <sergio@tiene.info>
2018-11-22 09:13:25 -0600

    Regresar a JSON normal en vez de JSON5
    JSON5 podra ser mas practico, pero JSON es el estandar y es mas
    compatible, por ejemplo, tiene mejor soporte de syntax highligting

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const receta = require('./receta');
 const fs = require('fs');
-const JSON5 = require('json5');
 
 fs.readFile('db/baberos.json', (err, data) => {
-    data = JSON5.parse(data);
+    data = JSON.parse(data);
     let baberos = new receta(data);
     if(baberos.preparar(10)) {
         baberos.reporte();
