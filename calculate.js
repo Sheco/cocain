4601392fafcad40cb1a24ae9c66097af2c513ea0
Sergio Duran <sergio@tiene.info>
2018-11-23 01:09:49 -0600

    Reorganize, the recipe is now a cost calculator module

---
+++ calculate.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const calculate = require('./costCalculator');
+const fs = require('fs');
+
+let source = process.argv[2];
+fs.promises.readFile(source)
+.then(data => JSON.parse(data))
+.then(data => calculate(data))
+//.then(data => JSON.stringify(data))
+.then(data => console.log(data))
+.catch(e => console.error(e));
+
