90a050925c2cb31141f0ff73e6cb9e35d9991731
Sergio Duran <sergio@tiene.info>
2018-11-22 19:48:13 -0600

    Make the index load a json file from an argument and return the results as a json string

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const recipe = require('./recipe');
 const fs = require('fs');
 
-fs.promises.readFile('db/stickers.json')
+let source = process.argv[2];
+fs.promises.readFile(source)
 .then(data => {
     data = recipe(JSON.parse(data));
-    console.log(data.resources);
-    console.log(`Total cost: $${data.cost.toFixed(2)}`);
+    console.log(JSON.stringify(data));
 });
