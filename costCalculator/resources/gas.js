29868973c733414e20724ad094e759cdc1df5acd
Sergio Duran <sergio@tiene.info>
2018-11-23 02:00:02 -0600

    Gas can now have a prepurchased amount, so the waste will be considered in the cost.

--- costCalculator/resources/gas.js
+++ costCalculator/resources/gas.js
@@ -1,6 +1,20 @@
+/* Gas
+ *
+ * mandatory variables:
+ * price: price per unit (liter/galon)
+ * mileage: vehicle's mileage
+ *
+ * optional variables:
+ * amount: the amount of gas you prepurchased, if there's any wasted gas
+ *  it will be considered in the cost
+ */
 module.exports = {
     unit: 'km',
     fixedCost: function(vars) {
+        if(vars.amount>0) {
+            return Math.round((vars.price/vars.mileage)
+                *vars.amount*100)/100;
+        }
         return vars.fixedCost || 0;
     },
     unitCost: function(vars) {
