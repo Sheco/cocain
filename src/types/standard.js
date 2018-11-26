60798327b0c92c171d8846d957d44dad0afe8d1d
Sergio Duran <sergio@tiene.info>
2018-11-25 18:19:59 -0600

    Improve the standard cost calculations
    If the object has a capacity, the cost is per container
    otherwise it is per unit consumed

--- src/types/standard.js
+++ src/types/standard.js
@@ -2,16 +2,25 @@
  *
  * Mandatory variables:
  * capacity: the capacity for each container of this resource
- * cost: cost for each container
+ * cost: if the object has a capacity, the cost is per container
+ *   otherwise, the cost is per unit consumed
  *
  * Optional variables:
  * amount: the maximum amount of containers available
  * type: the type of resource (defaults to 'standard')
  */
 module.exports = function (vars) {
+  if (!vars.capacity) {
+    return Math.round(((vars.cost * vars.consumed) || 0) * 100) / 100
+  }
+
+  /* if it doesn't have any waste and it has a capacity
+   * then it consumed all of the containers */
   if (vars.waste === undefined) {
-    vars.amount = Math.ceil(vars.consumed / (vars.capacity || 1))
+    vars.amount = Math.ceil(vars.consumed / vars.capacity)
     vars.waste = (vars.amount * vars.capacity) - vars.consumed
   }
+
+  /* if it has a capacity, the cost is that of the amount of containers */
   return Math.round(((vars.cost * vars.amount) || 0) * 100) / 100
 }
