9151e72f45dd176adc66a9b1275cac39ad6eb25d
Sergio Duran <sergio@tiene.info>
2018-11-28 16:39:08 -0600

    Update test suite

--- test.js
+++ test.js
@@ -11,9 +11,28 @@ async function assertJSON (file, result) {
     .then(data => calculate(data))
     .then(data => JSON.stringify(data))
     .then(data => assert.equal(data, result))
-    .catch(e => console.error(e))
+    .then(data => {
+      console.log(file + ' OK')
+    })
+    .catch(e => {
+      console.error(file + ' Fail')
+      console.error(e.message)
+    })
 }
 
+// test the csv convertor
+const convertCsv = require('./src/convertCsv')
+async function assertCsv (file, result) {
+  convertCsv(fs.createReadStream(file))
+    .then(data => {
+      assert.equal(JSON.stringify(data), result)
+      console.log(file + ' OK')
+    })
+    .catch(err => console.error(`${file} Failed\n${err}`))
+}
+assertCsv('samples/chocomilk.csv', '{"resources":[{"name":"milk","capacity":"1000","amount":"0","cost":"15"},{"name":"chocolate","capacity":"160","amount":"0","cost":"18.5"}],"setup":[],"product":[{"resource":"milk","amount":"240","":""},{"resource":"chocolate","amount":"15","":""}],"name":"chocomilk","amount":"60"}')
+
+// test the json samples
 // for f in samples/*; do echo "assertJSON('$f', '$(./calculate.js $f)')"; done 2>/dev/null
 assertJSON('samples/bibs.json', '{"products":10,"cost":76.96,"costPerProduct":7.7,"wastePcnt":4,"resources":[{"name":"fabric","amount":1,"cost":70,"consumed":14440,"left":560,"wastePcnt":4},{"type":"gas-per-distance","name":"gas","amount":2,"cost":2.81,"consumed":2},{"name":"john","amount":50,"cost":4.15,"consumed":50}]}')
 assertJSON('samples/chocoavena.json', '{"products":26,"cost":325.6,"costPerProduct":12.52,"wastePcnt":51.33,"resources":[{"name":"milk","amount":6,"cost":90,"consumed":5200,"left":800,"wastePcnt":80},{"name":"water","amount":15600,"cost":15.6,"consumed":15600},{"name":"sugar","amount":1,"cost":30,"consumed":390,"left":610,"wastePcnt":61},{"name":"chocoavena","amount":5,"cost":190,"consumed":1950,"left":50,"wastePcnt":13}]}')
