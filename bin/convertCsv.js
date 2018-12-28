b2825e3a588a14ea7b349182341f3d03a2f4c8a0
Sergio Duran <sergio@tiene.info>
2018-12-28 08:34:01 -0600

    Update the CSV convertor script

--- bin/convertCsv.js
+++ bin/convertCsv.js
@@ -7,11 +7,6 @@ const fs = require('fs')
 let source = process.argv[2]
 TransformCsv.csv(fs.createReadStream(source))
   .then(console.log)
-  /*
-  .then(JSON.parse)
-  .then(calculator)
-  .then(JSON.stringify)
-  .then(console.log)
-  .catch(console.error)
-  */
-
+  .catch(error => {
+    console.error(error.message)
+  })
