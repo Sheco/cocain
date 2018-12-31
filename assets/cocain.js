c078f614f73fc79820e6daed5bf4c6215fcfdedd
Sergio Duran <sergio@tiene.info>
2018-12-31 11:10:51 -0600

    Add buttons to download and upload the project

--- assets/cocain.js
+++ assets/cocain.js
@@ -79,3 +79,41 @@ if (sessionStorage.data === undefined) {
     products: []
   })
 }
+
+function download (filename, text, encoding) {
+  var element = document.createElement('a')
+  element.setAttribute('href', 'data:' + encoding + ';charset=utf-8,' +
+    text)
+  element.setAttribute('download', filename)
+
+  element.style.display = 'none'
+  document.body.appendChild(element)
+
+  element.click()
+
+  document.body.removeChild(element)
+}
+
+function downloadJSON () {
+  let filename = prompt('Name of the project')
+  if (!filename) return
+
+  download(filename + '.json', sessionStorage.data, 'application/json')
+  return false
+}
+
+function uploadJSON() {
+  let input = document.createElement('input')
+  input.setAttribute('type', 'file')
+
+  input.onchange = function (ev) {
+    let reader = new FileReader()
+    reader.onload = function (data) {
+      sessionStorage.data = data.target.result
+      window.location = '/'
+    }
+    reader.readAsText(ev.target.files[0])
+  }
+  input.click()
+  return false
+}
